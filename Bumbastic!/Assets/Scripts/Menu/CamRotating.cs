bb4972e8323574a40437188c61fc6e7993447404
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-03-09 17:54:06 -0500

    MenuWorld

---
+++ Bumbastic!/Assets/Scripts/Menu/CamRotating.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CamRotating : MonoBehaviour
+{
+    [SerializeField] Transform axis;
+    [SerializeField] float vel;
+
+    [SerializeField] GameObject principalCamera;
+    float elapsedTime = 0f;
+    private void Update()
+    {
+        if (principalCamera.activeInHierarchy)
+        {
+            elapsedTime += Time.deltaTime;
+            axis.eulerAngles = new Vector3(0, elapsedTime * vel, 0);
+        }
+    }
+}
