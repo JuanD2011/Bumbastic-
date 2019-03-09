6a1b7743c5f2dde67b211f3da8ffbba7df06f175
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-03-09 16:09:45 -0500

    Menu

--- Bumbastic!/Assets/Scripts/Menu/MenuUI.cs
+++ Bumbastic!/Assets/Scripts/Menu/MenuUI.cs
@@ -1,25 +1,16 @@
 ﻿using UnityEngine;
-using TMPro;
 
 public class MenuUI : MonoBehaviour
 {
     [SerializeField] Animator canvasAnimator;
     [SerializeField] Settings settings;
-    [SerializeField] TextMeshProUGUI menuNickname;
 
     public delegate void DelMenu();
-    public static DelMenu OnCompleteAnimation;
     public static DelMenu OnLoadData;
 
     public void CanLoadData()
     {
         OnLoadData?.Invoke();//Memento hears it.
-        menuNickname.text = settings.nickname;
-    }
-
-    public void AnimationSearchComplete()
-    {
-        OnCompleteAnimation?.Invoke();//LobbyBummie hears it.
     }
 
     public void IsSearchingGame(bool _bool)
@@ -29,22 +20,12 @@ public class MenuUI : MonoBehaviour
 
     public void ConfigurationPanel(bool _bool)
     {
-        canvasAnimator.SetBool("Configuration", _bool);
+        canvasAnimator.SetBool("Menu_Options", _bool);
     }
 
     public void CreditsPanel(bool _bool)
     {
         canvasAnimator.SetBool("Credits", _bool);
-    }
-
-    public void OkButton(bool _bool)
-    {
-        canvasAnimator.SetBool("OkButton", _bool);
-    }
-
-    public void NicknameSet()
-    {
-        canvasAnimator.SetTrigger("NicknameSet");
     }
 
     public void QuitPanel(bool _bool)
@@ -56,7 +37,7 @@ public class MenuUI : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (canvasAnimator.GetCurrentAnimatorStateInfo(0).IsName("PrincipalMenu"))
+            if (canvasAnimator.GetCurrentAnimatorStateInfo(0).IsName("Menu_Principal"))
             {
                 QuitPanel(true);
             }
--- Bumbastic!/Assets/Scripts/ScriptableObjects/Settings.cs
+++ Bumbastic!/Assets/Scripts/ScriptableObjects/Settings.cs
@@ -7,15 +7,5 @@ public class Settings : ScriptableObject
     #region ConfigurationSettings
     public bool isMusicActive;
     public bool isSfxActive;
-    public bool isJoysitckLocked;
     #endregion
-
-    [Header("Nickname")]
-    public bool isNicknameSet = false;
-    public string nickname;
-
-    public void Nickname(string _nickname)
-    {
-        nickname = _nickname;
-    }
 }
