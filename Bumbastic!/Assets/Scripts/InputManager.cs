5e455d2495e8a21b4a000b1c082a73d9d1d520b0
Juanchito <juan.atehortual@upb.edu.co>
2019-03-19 09:53:13 -0500

    PS Input

--- Bumbastic!/Assets/Scripts/InputManager.cs
+++ Bumbastic!/Assets/Scripts/InputManager.cs
@@ -123,47 +123,92 @@ public struct Controls
         {
             case 0:
                 controllerName = _controllerName;
-                aButton = "0AButton";
-                bButton = "0BButton";
-                startButton = "0StartButton";
-                ljoystickHorizontal = "0Horizontal";
-                ljoystickVertical = "0Vertical";
+                if (controllerName.Length > 31)
+                {
+                    aButton = "0AButton";
+                    bButton = "0BButton";
+                    startButton = "0StartButton";
+                    rightTrigger = "0RightTrigger";
+                    rjoystickHorizontal = "0RHorizontal";
+                }
+                else if (controllerName.Length > 18)
+                {
+                    aButton = "P0AButton";
+                    bButton = "P0BButton";
+                    startButton = "P0StartButton";
+                    rightTrigger = "P0RightTrigger";
+                    rjoystickHorizontal = "P0RHorizontal";
+                }
                 rjoystickVertical = "0RVertical";
-                rjoystickHorizontal = "0RHorizontal";
-                rightTrigger = "0RightTrigger";
+                ljoystickHorizontal = "0Horizontal";
+                ljoystickVertical = "0Vertical";  
                 break;
+
             case 1:
                 controllerName = _controllerName;
-                aButton = "1AButton";
-                bButton = "1BButton";
-                startButton = "1StartButton";
+                if (controllerName.Length > 31)
+                {
+                    aButton = "1AButton";
+                    bButton = "1BButton";
+                    startButton = "1StartButton";
+                    rightTrigger = "1RightTrigger";
+                    rjoystickHorizontal = "1RHorizontal";
+                }
+                else if (controllerName.Length > 18)
+                {
+                    aButton = "P1AButton";
+                    bButton = "P1BButton";
+                    startButton = "P1StartButton";
+                    rightTrigger = "P1RightTrigger";
+                    rjoystickHorizontal = "P1RHorizontal";
+                }
+                rjoystickVertical = "1RVertical";
                 ljoystickHorizontal = "1Horizontal";
                 ljoystickVertical = "1Vertical";
-                rjoystickVertical = "1RVertical";
-                rjoystickHorizontal = "1RHorizontal";
-                rightTrigger = "1RightTrigger";
                 break;
             case 2:
                 controllerName = _controllerName;
-                aButton = "2AButton";
-                bButton = "2BButton";
-                startButton = "2StartButton";
+                if (controllerName.Length > 31)
+                {
+                    aButton = "2AButton";
+                    bButton = "2BButton";
+                    startButton = "2StartButton";
+                    rightTrigger = "2RightTrigger";
+                    rjoystickHorizontal = "2RHorizontal";
+                }
+                else if (controllerName.Length > 18)
+                {
+                    aButton = "P2AButton";
+                    bButton = "P2BButton";
+                    startButton = "P2StartButton";
+                    rightTrigger = "P2RightTrigger";
+                    rjoystickHorizontal = "P2RHorizontal";
+                }
+                rjoystickVertical = "2RVertical";
                 ljoystickHorizontal = "2Horizontal";
                 ljoystickVertical = "2Vertical";
-                rjoystickVertical = "2RVertical";
-                rjoystickHorizontal = "2RHorizontal";
-                rightTrigger = "2RightTrigger";
                 break;
             case 3:
                 controllerName = _controllerName;
-                aButton = "3AButton";
-                bButton = "3BButton";
-                startButton = "3StartButton";
+                if (controllerName.Length > 31)
+                {
+                    aButton = "3AButton";
+                    bButton = "3BButton";
+                    startButton = "3StartButton";
+                    rightTrigger = "3RightTrigger";
+                    rjoystickHorizontal = "3RHorizontal";
+                }
+                else if (controllerName.Length > 18)
+                {
+                    aButton = "P3AButton";
+                    bButton = "P3BButton";
+                    startButton = "P3StartButton";
+                    rightTrigger = "P3RightTrigger";
+                    rjoystickHorizontal = "P3RHorizontal";
+                }
+                rjoystickVertical = "3RVertical";
                 ljoystickHorizontal = "3Horizontal";
                 ljoystickVertical = "3Vertical";
-                rjoystickVertical = "3RVertical";
-                rjoystickHorizontal = "3RHorizontal";
-                rightTrigger = "3RightTrigger";
                 break;
         }
     }
