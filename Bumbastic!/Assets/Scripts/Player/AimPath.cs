5377c9913954a7f010e12073dacf92ac94fc826c
Jorge Orrego <jorgeandres0111@gmail.com>
2019-03-19 19:46:26 -0500

    Aim

--- Bumbastic!/Assets/Scripts/Player/AimPath.cs
+++ Bumbastic!/Assets/Scripts/Player/AimPath.cs
@@ -6,6 +6,9 @@ public class AimPath : MonoBehaviour
     LineRenderer m_LineRenderer;
     private float targetRotation;
 
+    [SerializeField] Transform axis;
+    private float turnSmoothVel;
+
     void Start()
     {
         m_Player = GetComponentInParent<Player>();
@@ -14,14 +17,24 @@ public class AimPath : MonoBehaviour
 
     private void Update()
     {
-        if (m_Player.InputAiming != Vector2.zero)
+        if (m_Player.HasBomb)
         {
-            transform.LookAt(m_Player.InputAiming);
-            m_LineRenderer.enabled = true;
-        }
-        else
-        {
-            m_LineRenderer.enabled = false;
+            if (m_Player.InputAiming != Vector2.zero)
+            {
+                if (!m_LineRenderer.enabled)
+                {
+                    m_LineRenderer.enabled = true;
+                }
+                targetRotation = Mathf.Atan2(m_Player.InputAiming.x, m_Player.InputAiming.y) * Mathf.Rad2Deg;
+                axis.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(axis.eulerAngles.y, targetRotation, ref turnSmoothVel, m_Player.TurnSmooth);
+            }
+            else
+            {
+                if (m_LineRenderer.enabled)
+                {
+                    m_LineRenderer.enabled = false;
+                }
+            }
         }
     }
 }
