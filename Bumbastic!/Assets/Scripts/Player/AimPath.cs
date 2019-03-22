b5f362d575ac4958f58fa0a5871ac5437fa35631
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-03-21 20:46:12 -0500

    Update AimPath.cs

--- Bumbastic!/Assets/Scripts/Player/AimPath.cs
+++ Bumbastic!/Assets/Scripts/Player/AimPath.cs
@@ -24,7 +24,7 @@ public class AimPath : MonoBehaviour
             {
                 m_LineRenderer.enabled = true;
             }
-            targetRotation = Mathf.Atan2(aimNormalized.x, aimNormalized.y) * Mathf.Rad2Deg;
+            targetRotation = -1f * Mathf.Atan2(aimNormalized.x, aimNormalized.y) * Mathf.Rad2Deg;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVel, m_Player.TurnSmooth);
         }
         else
