8f04845aaf4c5cf8011bd87cb9309b080e475a88
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-04-02 12:39:06 -0500

    Update PowerUp.cs

--- Bumbastic!/Assets/Scripts/PowerUps/PowerUp.cs
+++ Bumbastic!/Assets/Scripts/PowerUps/PowerUp.cs
@@ -12,26 +12,23 @@ public class PowerUp : MonoBehaviour, IPowerUp
 
     public void PickPowerUp(Player _player)
     {
-        if (!_player.HasBomb)
+        int randomPU = Random.Range(0, 3);
+        switch (randomPU)
         {
-            int randomPU = Random.Range(0, 3);
-            switch (randomPU)
-            {
-                case 0:
-                    _player.gameObject.AddComponent<Velocity>();
-                    break;
-                case 1:
-                    _player.gameObject.AddComponent<Magnet>();
-                    break;
-                case 2:
-                    _player.gameObject.AddComponent<Shield>();
-                    break;
-                case 3:
-                    //GameManager.instance.bombHolder.gameObject.AddComponent<Velocity>();
-                    break;
-                default:
-                    break;
-            }
+            case 0:
+                _player.gameObject.AddComponent<Velocity>();
+                break;
+            case 1:
+                _player.gameObject.AddComponent<Magnet>();
+                break;
+            case 2:
+                _player.gameObject.AddComponent<Shield>();
+                break;
+            case 3:
+                //GameManager.instance.bombHolder.gameObject.AddComponent<Velocity>();
+                break;
+            default:
+                break;
         }
     }
 }
