dc801a64b978f049a5acf58061d297643371c868
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-04-15 13:15:46 -0500

    GameMode Class

---
+++ Bumbastic!/Assets/Scripts/GameModes/GameMode.cs
@@ -0,0 +1,14 @@
+﻿[System.Serializable]
+public class GameMode
+{
+    public string name;
+    public string description;
+    public int id;
+
+    public GameMode(string _name, string _description, int _id)
+    {
+        name = _name;
+        description = _description;
+        id = _id;
+    }
+}
--- Bumbastic!/Assets/Scripts/Skins/Skin.cs
+++ Bumbastic!/Assets/Scripts/Skins/Skin.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-[Serializable]
+[System.Serializable]
 public class Skin
 {
     public string name;
