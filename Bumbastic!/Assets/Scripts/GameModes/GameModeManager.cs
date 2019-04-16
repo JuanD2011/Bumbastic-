072f2cb82010eb8a229e47eb72377a19d4bf06c8
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-04-15 19:57:24 -0500

    Update GameModeManager.cs

--- Bumbastic!/Assets/Scripts/GameModes/GameModeManager.cs
+++ Bumbastic!/Assets/Scripts/GameModes/GameModeManager.cs
@@ -8,6 +8,11 @@ public class GameModeManager : MonoBehaviour
     public delegate IEnumerator DelCanvasGameMode(string _sceneToLoad);
     public static DelCanvasGameMode OnLoadScene;
 
+    private void Awake()
+    {
+        OnLoadScene = null;
+    }
+
     private void Start()
     {
         Invoke("SceneToLoad", timeToLoad);
