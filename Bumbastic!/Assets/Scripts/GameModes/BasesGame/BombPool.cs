009e0c835f021bd8d4741120f571755b8a62d93b
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-04-23 20:05:55 -0500

    Bomb Spawner & pool

---
+++ Bumbastic!/Assets/Scripts/GameModes/BasesGame/BombPool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPool : MonoBehaviour
+{
+    public static BombPool instance;
+
+    [Tooltip("Use BombBaseGame")]
+    [SerializeField] GameObject bombTemplate;
+
+    [Header("Bombs to instantiate")]
+    [Range(2, 10)]
+    [SerializeField] int bombsInScene = 10;
+
+    List<Bomb> bombsInGame = new List<Bomb>();
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this);
+
+        CreateBombs();
+    }
+
+    private void CreateBombs()
+    {
+        for (int i = 0; i < bombsInScene; i++)
+        {
+            GameObject bombClone = Instantiate(bombTemplate, Vector3.zero, Quaternion.identity);
+            bombClone.name = string.Format("{0} bomb", i);
+            bombClone.SetActive(false);
+            bombsInGame.Add(bombClone.GetComponent<Bomb>());
+        }
+    }
+
+    public Bomb GetAvailableBomb()
+    {
+        Bomb result;
+
+        for (int i = 0; i < bombsInGame.Count; i++)
+        {
+            if (!bombsInGame[i].gameObject.activeInHierarchy)
+            {
+                return result = bombsInGame[i];
+                break;
+            }
+        }
+
+        GameObject bombGO = Instantiate(bombTemplate, Vector3.zero, Quaternion.identity);
+        result = bombGO.GetComponent<Bomb>();
+        bombsInGame.Add(result);
+        bombGO.name = string.Format("{0} bomb", bombsInGame.Count - 1);
+
+        return result;
+    }
+}
---
+++ Bumbastic!/Assets/Scripts/GameModes/BasesGame/BombSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+    [SerializeField] float force = 1f;
+    [SerializeField] float timeToSpawnBomb = 5f;
+    [SerializeField] float xRandom = 25f, zRandom = 25f;
+
+    Collider spawner;
+
+    Bomb bombToSpawn;
+
+    private void Start()
+    {
+        spawner = GetComponent<Collider>();
+
+        InvokeRepeating("SpawnBomb", timeToSpawnBomb, timeToSpawnBomb);
+    }
+
+    private void SpawnBomb()
+    {
+        bombToSpawn = BombPool.instance.GetAvailableBomb();
+        bombToSpawn.transform.position = spawner.GetPointInVolume();
+        bombToSpawn.RigidBody.AddForce(GetRandomVector() * force, ForceMode.Impulse);
+    }
+
+    private Vector3 GetRandomVector()
+    {
+        Vector3 result = Vector3.up;
+
+        result = new Vector3(Random.Range(-xRandom, xRandom), 1f, Random.Range(-zRandom, zRandom));
+
+        return result;
+    }
+}
+
+public static class SpawnerExtensions
+{
+    public static Vector3 GetPointInVolume(this Collider collider)
+    {
+        Vector3 result = Vector3.zero;
+        result = new Vector3(Random.Range(collider.bounds.min.x, collider.bounds.max.x), collider.transform.position.y, 0F);
+
+        return result;
+    }
+}
