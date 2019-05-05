2ce79ed0e587897bcdd78d6c7f4989733ee266a4
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-05-05 16:44:19 -0500

    [WIP] wick
    Falta pulir shader de wick, partículas readys, al igual que su movimiento

---
+++ Bumbastic!/Assets/Scripts/Bomb/Wick.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Wick : MonoBehaviour
+{
+    [SerializeField] Transform particles;
+    [SerializeField] Transform[] points;
+
+    Bomb bomb;
+
+    Renderer renderer;
+
+    private void Start()
+    {
+        bomb = GetComponentInParent<Bomb>();
+        renderer = GetComponent<Renderer>();
+
+        StartCoroutine(WickMovement());
+        Bomb.OnExplode += ResetWick;
+    }
+
+    private void ResetWick()
+    {
+        StartCoroutine(WickMovement());
+    }
+
+    IEnumerator WickMovement()
+    {
+        int currentPoint = 0;
+        float timePerPoint;
+        float elapsedTime = 0f;
+        timePerPoint = bomb.Timer / (points.Length - 1);
+        Debug.Log(bomb.Timer);
+        Debug.Log(timePerPoint);
+        particles.position = points[0].position;
+
+        yield return new WaitUntil(() => !bomb.Exploded);
+
+        while (!bomb.Exploded)
+        {
+            if (bomb.CanCount)
+            {
+                float distance = Vector3.Distance(points[currentPoint].position, particles.position);
+                particles.position = Vector3.MoveTowards(particles.position, points[currentPoint].position, (Time.deltaTime * distance) / timePerPoint);
+                renderer.material.SetFloat("_Factor", 1f - elapsedTime / bomb.Timer);
+
+                if (distance < 0.1f && currentPoint < points.Length - 1)
+                {
+                    currentPoint++;
+                    Debug.Log(currentPoint);
+                }
+                elapsedTime += Time.deltaTime; 
+            }
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Gizmos.color = Color.red;
+            Vector3 position = points[i].position;
+            Gizmos.DrawSphere(position, 0.05f);
+
+            if (i > 0)
+            {
+                Vector3 previous = points[i - 1].position;
+                Gizmos.DrawLine(previous, position);
+            }
+        }
+    }
+}
---
+++ Bumbastic!/Assets/Shaders/Wick.shader
@@ -0,0 +1,60 @@
+﻿Shader "Custom/Wick"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+
+		//Alpha
+		_AlphaMask("Alpha mask", 2D) = "white" {}
+		_Factor("Alpha Factor", Range(0, 1)) = 0.5
+		_Treshold("Treshold", Range(0, 1)) = 0.5
+    }
+    SubShader
+    {
+        Tags 
+		{
+			"Queue" = "Transparent"
+			"IgnoreProjector" = "True"
+			"RenderType" = "TransparentCutout"
+		}
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Standard fullforwardshadows alpha:fade
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+		sampler2D _AlphaMask;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        fixed4 _Color;
+		float _Factor;
+		float _Treshold;
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			fixed4 mask = tex2D(_AlphaMask, IN.uv_MainTex);
+
+			o.Albedo = c.rgb;
+
+			fixed alpha = _Factor * mask;
+			
+			if (alpha < _Treshold)
+			{
+				o.Alpha = 0;
+			}
+			else 
+			{
+				o.Alpha = 1;
+			}
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
