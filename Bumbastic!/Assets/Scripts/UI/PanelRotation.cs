264698f4ad154b1716d9740e252586a51a5a28ef
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-07-12 10:07:16 -0500

    Panel

---
+++ Bumbastic!/Assets/Scripts/UI/PanelRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PanelRotation : MonoBehaviour
+{
+    [SerializeField] Vector3 vector3 = Vector3.forward;
+    [SerializeField] private float speed = 1f;
+
+    void Update()
+    {
+        transform.eulerAngles += vector3 * speed * Time.deltaTime;
+    }
+}
