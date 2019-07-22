79c5a398a1240dedcc48374fb574ff37052a63a9
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-07-22 15:31:44 -0500

    Cleaning and optimization

--- Bumbastic!/Assets/Scripts/Audio/AudioClips.cs
+++ Bumbastic!/Assets/Scripts/Audio/AudioClips.cs
@@ -8,21 +8,30 @@ public class AudioClips : ScriptableObject
     public AudioClip transitionMusic;
 
     [Header("Ambient")]
-    public AudioClip desertAmbient, winterAmbient;
+    public AudioClip desertAmbient;
+    public AudioClip winterAmbient;
 
     [Header("SFx")]
-    public AudioClip bumba, go, mrBumbasticIs;
+    public AudioClip bumba;
+    public AudioClip go;
+    public AudioClip mrBumbasticIs;
     public AudioClip cLose, cWin, cTransmitter, cStun, cThrow, cSpeedUP;
-    public AudioClip bomb, confettiBomb;
-    public AudioClip bombThrow, bombReception;
-    public AudioClip buttonDefault, buttonBack, buttonSelection;
+    public AudioClip bomb;
+    public AudioClip confettiBomb;
+    public AudioClip bombThrow;
+    public AudioClip bombReception;
+    public AudioClip buttonDefault;
+    public AudioClip buttonBack;
+    public AudioClip buttonSelection;
     public AudioClip crow;
     public AudioClip powerUpBoxDropped;
-    public AudioClip dropModule, anticipation;
+    public AudioClip dropModule;
+    public AudioClip anticipation;
     public AudioClip stun;
-    public AudioClip wagonHit, rollingWagon;
+    public AudioClip wagonHit;
+    public AudioClip rollingWagon;
     public AudioClip speedUP;
-    public AudioClip dash = null;
+    public AudioClip dash;
 
     [Header("Support")]
     public AudioClip crowdCheer;
