5d0d295f4bfcc18f1efd05a4461dd8d8daeb0b6a
JuanD2011 <juan.atehortual@upb.edu.co>
2019-08-10 19:35:25 -0500

    Update InputManager.cs

--- Bumbastic!/Assets/Scripts/Input/InputManager.cs
+++ Bumbastic!/Assets/Scripts/Input/InputManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public static InputManager inputManager;
+    byte counter = 0;
 
     public event System.Action OnDeviceDisconnected, OnDeviceReconnected;
     public event System.Action OnDeviceAdded;
@@ -25,20 +26,23 @@ public class InputManager : MonoBehaviour
 
     private void UpdateGamepadState(InputDevice _device, InputDeviceChange _change)
     {
-        List<InputDevice> inputList = new List<InputDevice>();
-
-        foreach (PlayerMenu item in MenuManager.menu.Players)
-        {
-            inputList.Add(item.InputDevice);
-        }
-
         if (Application.isPlaying)
         {
+            List<InputDevice> inputList = new List<InputDevice>();
+
+            foreach (PlayerMenu item in MenuManager.menu.Players)
+            {
+                inputList.Add(item.InputDevice);
+            }
+
             switch (_change)
             {
                 case InputDeviceChange.Added:
-                    if (!inputList.Contains(_device)) OnDeviceAdded?.Invoke();
-                    Debug.Log("Device added with id: " + (_device.id));
+                    if (!inputList.Contains(_device))
+                    {
+                        OnDeviceAdded?.Invoke();
+                        Debug.Log("Device added with id: " + (_device.id));
+                    }
                     break;
                 case InputDeviceChange.Removed:
                     //Device completely removed
@@ -53,12 +57,12 @@ public class InputManager : MonoBehaviour
                     break;
                 default:
                     break;
-            }   
+            }
         }
     }
 
     private void GetGamepads()
-    { 
+    {
         int gamepadCount = Gamepad.all.Count;
         MenuManager.menu.InitializeFirstPlayers(gamepadCount);
     }
