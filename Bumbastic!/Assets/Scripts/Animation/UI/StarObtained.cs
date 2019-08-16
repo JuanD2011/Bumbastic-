7eb75dffc549c2a572158e41ca3ecc3e1479be2a
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-08-16 18:02:44 -0500

    [WIP] Vfx star obtained

--- Bumbastic!/Assets/Scripts/Animation/UI/StarObtained.cs
+++ Bumbastic!/Assets/Scripts/Animation/UI/StarObtained.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class StarObtained : BaseAnimationUI
 {
+    ParticleSystem cParticleSystem = null;
+    [SerializeField] private float rotVel = 10f;
+
     protected override void Awake()
     {
         base.Awake();
+        cParticleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Start()
     {
         StartCoroutine(SyncSFXAndVFX());
+    }
+
+    private void Update()
+    {
+        cParticleSystem.transform.eulerAngles += Vector3.forward * Time.deltaTime * rotVel;
     }
 
     IEnumerator SyncSFXAndVFX()
